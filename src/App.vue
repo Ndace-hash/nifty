08eda5e038bf1817938be03c0745c6c2f7983b19
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-11-22 00:50:56 +0100

    basic layout structure

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,204 @@
 </script>
 
 <template>
-  <p>hello</p>
+  <div class="box py-4">
+    <div class="flex text-white">
+      <p>Edit sequence initiated</p>
+      <p>Edit sequence initiated</p>
+      <p>Edit sequence initiated</p>
+    </div>
+  </div>
+  <section class="box my-4 relative h-[500px] overflow-hidden">
+    <img src="./assets/images/sky-city.jpg" alt="sky-city" class="w-full h-full object-cover">
+    <div class="border border-white absolute right-0 top-0 rounded-lg rounded-tl-none rounded-br-none bg-[#434343]">
+      <div class="flex">
+        <div class="border border-x-white px-4">hello</div>
+        <div class="border border-x-white px-4">hello</div>
+        <div class="border border-x-white px-4">hello</div>
+        <div class="border border-x-white px-4">hello</div>
+      </div>
+      <div class="flex">
+        <div class="border-x-white border px-8">hello</div>
+        <div class="flex flex-col justify-between gap-y-12">
+          <h2 class="uppercase">Initiate sequence</h2>
+          <h2 class="capitalize text-sm text-right">do something <br />to get this</h2>
+        </div>
+      </div>
+    </div>
+    <button
+      class="absolute bottom-0 left-0 bg-white rounded-lg px-6 py-1 uppercase rounded-br-none rounded-tl-none text-black">click
+      to
+      mint</button>
+  </section>
+  <div class="box py-4">
+    <div class="flex text-white">
+      <p>Edit sequence initiated</p>
+      <p>Edit sequence initiated</p>
+      <p>Edit sequence initiated</p>
+    </div>
+  </div>
+  <section class="box flex my-4 h-[300px]">
+    <div class="flex flex-col justify-between">
+      <div class="uppercase about">
+        <p>The Nifty portal is an Nft art piece designed by <span>thoma dubois</span> with music by <span>gramatis</span>
+          and
+          <span>llmas</span>.
+        </p>
+        <p>it serves as your premium membership pass into <span>the nifty community</span>, a group of aspiring and
+          successful nft collectors and creators</p>
+      </div>
+      <div class="border-t border-white flex w-full">
+        <div class="border-r border-white"> hello</div>
+        <div class="border-r border-white"> hello</div>
+        <div class="border-r border-white"> hello</div>
+        <div class="border-r border-white"> hello</div>
+      </div>
+    </div>
+    <div class="border-x px-4">
+      <h1>Breaking <br /> News</h1>
+      <h1>Breaking <br /> News</h1>
+      <h1>Breaking <br /> News</h1>
+    </div>
+    <div>
+      <div class="border-b border-white">
+        <div class="border-2 border-white rounded-full w-[50px] h-[50px]">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="box flex overflow-hidden">
+    <div class="w-3/5 h-[400px]">
+      <img src="./assets/images/sky-city.jpg" alt="" class="w-full h-full">
+    </div>
+    <div class="w-full">
+      <div class="flex">
+        <div>
+          <div class="flex gap-[5px]">
+
+            <span v-for="n of 10" class="h-full w-[5px] bg-white block"></span>
+          </div>
+        </div>
+        <div>
+          <p>002 // how it works</p>
+        </div>
+      </div>
+      <div class="w-full">
+        <p class="text-right">the nifty portal is mining <br />in three phases</p>
+      </div>
+      <div class="w-full">
+        <p class="text-right">the nifty portal is mining <br />in three phases</p>
+      </div>
+      <div class="w-full">
+        <p class="text-right">the nifty portal is mining <br />in three phases</p>
+      </div>
+    </div>
+  </section>
+  <section class="box my-4 min-h-[400px]">
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="flex h-full">
+      <div class="flex-grow">
+        <div class="border-b border-white flex items-center justify-center">
+          <div class="w-[300px] h-[300px] rounded-full border border-white relative flex items-center justify-center">
+            <div class="w-[80%] h-[80%] rounded-full border border-white flex items-center justify-center">
+              <div class="w-[70%] h-[70%] rounded-full border border-white flex items-center justify-center">
+                <div class="w-[60%] h-[60%] rounded-full border border-white flex items-center justify-center">
+                  <div class="w-[50%] h-[50%] rounded-full border border-white flex items-center justify-center">
+                    <div class="w-[40%] h-[40%] rounded-full border border-white flex items-center justify-center"></div>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>member discord</h3>
+          <p>exclusive access to the members-<br />only area of our discord</p>
+        </div>
+      </div>
+      <div class="border-x border-white flex-grow">
+        <div class="border-b border-white py-3 uppercase">
+          <h3>Early access</h3>
+          <p>early minting of our upcoming projects</p>
+        </div>
+        <div>
+          <div class="border-b border-white flex items-center justify-center">
+            <div class="w-[300px] h-[300px] rounded-2xl border border-white relative flex items-center justify-center">
+              <div class="w-[80%] h-[80%] rounded-2xl border border-white flex items-center justify-center">
+                <div class="w-[70%] h-[70%] rounded-2xl border border-white flex items-center justify-center">
+                  <div class="w-[60%] h-[60%] rounded-2xl border border-white flex items-center justify-center">
+                    <div class="w-[50%] h-[50%] rounded-2xl border border-white flex items-center justify-center">
+                      <div class="w-[40%] h-[40%] rounded-2xl border border-white flex items-center justify-center">
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+  </section>
+  <section class="box h-[600px] relative flex items-center justify-center">
+    <img src="./assets/images/sky-city.jpg" alt="" class="object-cover w-full h-full">
+    <div class="border-b border-white flex items-center justify-center absolute">
+      <div class="w-[300px] h-[300px] rounded-2xl border border-white relative flex items-center justify-center">
+        <div class="w-[80%] h-[80%] rounded-2xl border border-white flex items-center justify-center">
+          <div class="w-[70%] h-[70%] rounded-2xl border border-white flex items-center justify-center">
+            <div class="w-[60%] h-[60%] rounded-2xl border border-white flex items-center justify-center">
+              <div class="w-[50%] h-[50%] rounded-2xl border border-white flex items-center justify-center">
+                <div class="w-[40%] h-[40%] rounded-2xl border border-white flex items-center justify-center">
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="box py-4 my-4">
+    <div class="flex text-white">
+      <p>Edit sequence initiated</p>
+      <p>Edit sequence initiated</p>
+      <p>Edit sequence initiated</p>
+    </div>
+  </div>
 </template>
 
+
+<style scoped>
+.about p span {
+  background-color: white;
+  color: black;
+}
+</style>

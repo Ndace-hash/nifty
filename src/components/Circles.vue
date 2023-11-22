75f9a340efb9493d6875e5038618ff50eefd07fc
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-11-22 17:24:34 +0100

    seperated animated shapes into components

---
+++ src/components/Circles.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="border-b border-white flex items-center justify-center">
+        <div class="w-[300px] h-[300px] rounded-full border border-white relative flex items-center justify-center">
+            <div class="w-[80%] h-[80%] rounded-full border border-white flex items-center justify-center">
+                <div class="w-[70%] h-[70%] rounded-full border border-white flex items-center justify-center">
+                    <div class="w-[60%] h-[60%] rounded-full border border-white flex items-center justify-center">
+                        <div class="w-[50%] h-[50%] rounded-full border border-white flex items-center justify-center">
+                            <div class="w-[40%] h-[40%] rounded-full border border-white flex items-center justify-center">
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
---
+++ src/components/Squares.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="border-b border-white flex items-center justify-center">
+        <div class="w-[300px] h-[300px] rounded-2xl border border-white relative flex items-center justify-center">
+            <div class="w-[80%] h-[80%] rounded-2xl border border-white flex items-center justify-center">
+                <div class="w-[70%] h-[70%] rounded-2xl border border-white flex items-center justify-center">
+                    <div class="w-[60%] h-[60%] rounded-2xl border border-white flex items-center justify-center">
+                        <div class="w-[50%] h-[50%] rounded-2xl border border-white flex items-center justify-center">
+                            <div class="w-[40%] h-[40%] rounded-2xl border border-white flex items-center justify-center">
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>

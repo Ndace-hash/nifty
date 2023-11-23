fc72935462f080df36af762f40303ebd4c7f1b75
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-11-23 12:08:16 +0100

    square animation

--- src/components/Squares.vue
+++ src/components/Squares.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border-b border-white flex items-center justify-center">
+    <div class=" flex items-center justify-center" ref="obj">
         <div class="w-[300px] h-[300px] rounded-2xl border border-white relative flex items-center justify-center">
             <div class="w-[80%] h-[80%] rounded-2xl border border-white flex items-center justify-center">
                 <div class="w-[70%] h-[70%] rounded-2xl border border-white flex items-center justify-center">
@@ -19,5 +19,29 @@
     </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+const obj = ref(null)
+onMounted(() => {
+    const inView = ref(ScrollTrigger.isInViewport(obj.value, 1))
+
+
+    ScrollTrigger.observe({
+        target: window,
+        type: "wheel,touch,pointer,scroll",
+        onDown() {
+            gsap.to(obj.value, {
+                scale: '3'
+            })
+        },
+        onUp() {
+            gsap.to(obj.value, {
+                scale: 0
+            })
+        }
+    })
+})
 </script>
